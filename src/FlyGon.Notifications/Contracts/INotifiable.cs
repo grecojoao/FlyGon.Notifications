646975950978845e07b03235db7d13836aaf7da1
João Greco <junior.greco@hotmail.com>
2021-05-29 18:04:28 -0300

    Implementação do Pattern e do Projeto "How to use"

---
+++ src/FlyGon.Notifications/Contracts/INotifiable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace FlyGon.Notifications.Contracts
+{
+    internal interface INotifiable
+    {
+        IReadOnlyCollection<Notification> Notifications { get; }
+        bool IsInvalid { get; }
+        bool IsValid { get; }
+        string NotificationsMessage();
+    }
+}
